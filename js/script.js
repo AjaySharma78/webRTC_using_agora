01402dcd81864d64048e42ae1c335239464aa36e
Ajay sharma <aakirtsharma123@gmail.com>
2024-05-03 01:59:40 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+toggle.style.right = "-180px";
+function menu() {
+    if (toggle.style.right == "0px") {
+        toggle.style.right= "-180px";
+    }
+    else {
+        toggle.style.right = "0px";
+    }
+
+}
